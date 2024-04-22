f3a7c26757c8291c62a71c832103afce52822543
bachokhachvani <khachvanis@gmail.com>
2024-04-22 18:50:39 +0400

    added auth controller

---
+++ src/main/java/com/bachokhachvani/employeemanagementapp/controllers/AuthController.java
@@ -0,0 +1,48 @@
+package com.bachokhachvani.employeemanagementapp.controllers;
+
+import com.bachokhachvani.employeemanagementapp.domain.AuthResponseDTO;
+import com.bachokhachvani.employeemanagementapp.domain.LoginDTO;
+import com.bachokhachvani.employeemanagementapp.domain.UserDTO;
+import com.bachokhachvani.employeemanagementapp.models.UserModel;
+import com.bachokhachvani.employeemanagementapp.security.TokenGenerator;
+import com.bachokhachvani.employeemanagementapp.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class AuthController {
+
+    private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private TokenGenerator tokenGenerator;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<UserModel> Register(@RequestBody UserDTO user) {
+        UserModel userModel = new UserModel();
+        userModel.setUsername(user.getUsername());
+        userModel.setPassword(user.getPassword());
+
+        UserModel savedUser = userService.addUser(userModel, user.getRoleName());
+        return ResponseEntity.ok(savedUser);
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO login) {
+
+        var authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = tokenGenerator.generateToken(authentication);
+
+        return ResponseEntity.ok(new AuthResponseDTO(token));
+    }
+
+}

7b173000cf4829ebd4de85d4232e5f5ecc58a3d6
bachokhachvani <khachvanis@gmail.com>
2024-05-08 16:51:36 +0400

    added more validation to employeeDTO

--- src/main/java/com/bachokhachvani/employeemanagementapp/domain/EmployeeDTO.java
+++ src/main/java/com/bachokhachvani/employeemanagementapp/domain/EmployeeDTO.java
@@ -1,9 +1,6 @@
 package com.bachokhachvani.employeemanagementapp.domain;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.sql.Date;
@@ -15,21 +12,18 @@ public class EmployeeDTO {
     @NotBlank(message = "Username must not be empty")
     @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters long")
     private String name;
-
     @Min(value = 100000, message = "Phone number must be at least 6 digits long")
     private Integer phone;
-
+    @Past(message = "Birthday is not valid")
     private Date birthday;
-
     @Email(message = "Email should be valid")
     private String email;
+    @PastOrPresent(message = "Hire Date is not Valid")
     private Date hireDate;
+    @Positive(message = "Salary is not Valid")
     private Double salary;
     private String department;
-
-
     private String position;
-
     @Email(message = "Email should be valid")
     private String managerEmail;
     private String managerName;
